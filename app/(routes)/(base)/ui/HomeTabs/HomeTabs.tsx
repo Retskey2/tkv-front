a7f36cdb280eb040801c88a82b9058e316c6ef1d
Retskey2 <bashlovka96@mail.ru>
2025-12-10 03:07:58 +0300

    added stiky on list

--- app/(routes)/(base)/ui/HomeTabs/HomeTabs.tsx
+++ app/(routes)/(base)/ui/HomeTabs/HomeTabs.tsx
@@ -1,16 +1,42 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import { List } from "@/shared/ui/List/List";
 import TabCarousel from "@/shared/ui/TabCarousel/TabCarousel";
 import styles from "./HomeTabs.module.scss";
 
 export const HomeTabs = () => {
+  const tabRef = useRef<HTMLDivElement>(null);
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsSticky(!entry.isIntersecting);
+      },
+      { threshold: 1 }
+    );
+
+    if (tabRef.current) observer.observe(tabRef.current);
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className={styles["container"]}>
-      <TabCarousel
-        tabs={["Holders leaderboard", "Latest transfers", "TOP users"]}
-        onChange={(index) => console.log("Selected:", index)}
-      />
+      <div ref={tabRef} />
+
+      <div
+        className={`${styles["wrapper-sticky"]} ${
+          isSticky && styles["active"]
+        }`}
+      >
+        <TabCarousel
+          tabs={["Holders leaderboard", "Latest transfers", "TOP users"]}
+          onChange={(index) => console.log("Selected:", index)}
+        />
+      </div>
+
       <div className={styles["wrapper-list"]}>
         <List />
       </div>
